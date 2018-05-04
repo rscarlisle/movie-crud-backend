c140c8709f37b9401d3f1d3653a19ac799e41fb2
Randall Carlisle <cisolutionsinc@gmail.com>
2018-05-04 12:57:49 -0700

    added env variable and required knexfile and knex/config

---
+++ queries/main.js
@@ -0,0 +1,44 @@
+const knex = require('./db')
+
+const getAll = () => {
+  return knex('movies')
+    .select('')
+}
+
+const getMovie = (id) => {
+
+  return knex('movies')
+    .where({ id })
+} 
+
+
+const addMovie = (id, title, director, year, rating, url) => {
+
+  return knex
+    .insert( { id, title, director, year, rating, url } )
+    .into('movies')
+    .returning('*')
+}
+
+const saveMovieDetails = (id, title, director, year, rating, url) => {
+
+  return knex('movies')
+    .where({ id })
+    .update( { title, director, year, rating, url } )
+    .returning('*')
+}
+
+const deleteMovie = (id) => {
+  return knex('movies')
+    .where({ id })
+    .del()
+    .returning('*')
+}
+
+module.exports = {
+  getAll,
+  getMovie,
+  addMovie,
+  saveMovieDetails,
+  deleteMovie
+}
